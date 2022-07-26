fc2878de386bc66712486fa68567d745aa28adfc
Ruiyot Abby <ruiyotabby@gmail.com>
2022-07-26 14:18:02 +0300

    commit
    Signed-off-by: Ruiyot Abby <ruiyotabby@gmail.com>

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:commerce_app/home.dart';
+import 'package:flutter/material.dart';
+
+class CommerceApp extends StatelessWidget {
+  const CommerceApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Commerce',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+      },
+    );
+  }
+}
