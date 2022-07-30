d938049ca623fc51f1e498d6afb2fbf4262685b5
Ruiyot Abby <ruiyotabby@gmail.com>
2022-07-30 10:00:20 +0300

    .
    Signed-off-by: Ruiyot Abby <ruiyotabby@gmail.com>

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -8,6 +8,7 @@ class RegisterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
+    
     return DefaultTabController(
       length: 2,
       child: Scaffold(
