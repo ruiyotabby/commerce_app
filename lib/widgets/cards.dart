91d2b084bd8f99c64d2cd380f8cff7fb91f07e97
Ruiyot Abby <ruiyotabby@gmail.com>
2022-07-30 14:58:51 +0300

    card
    Signed-off-by: Ruiyot Abby <ruiyotabby@gmail.com>

--- lib/widgets/cards.dart
+++ lib/widgets/cards.dart
@@ -139,3 +139,17 @@ class GridViewWidget extends StatelessWidget {
     );
   }
 }
+
+class ItemCard extends StatefulWidget {
+  const ItemCard({Key? key}) : super(key: key);
+
+  @override
+  State<ItemCard> createState() => _ItemCardState();
+}
+
+class _ItemCardState extends State<ItemCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
