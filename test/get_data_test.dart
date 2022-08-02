8abb1840ae86eb57c9682ae2852d9bc9bd3b1f32
Ruiyot Abby <ruiyotabby@gmail.com>
2022-08-02 22:24:29 +0300

    t
    Signed-off-by: Ruiyot Abby <ruiyotabby@gmail.com>

--- test/get_data_test.dart
+++ test/get_data_test.dart
@@ -1,5 +1,5 @@
 import 'package:commerce_app/models/products.dart';
-import 'package:commerce_app/services/get_data.dart';
+import 'package:commerce_app/services/fetch_products.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/annotations.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -14,16 +14,16 @@ void main() {
       final client = MockClient();
       when(client.get(Uri.parse('https://fakestoreapi.com/products')))
           .thenAnswer((_) async => http.Response(
-              '{"id":1, "title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops","price":109.95,"description":"Your perfect pack for everyday use and walks in the forest, stash your laptop (up to 15 inches) in the padded sleeve, your everyday","category":"men\'s clothing","image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg","rating":{"rate":3.9,"count":120}}',
+              '{"id":1, "title":"Fjallraven","price":109.95,"description":"Your perfect pack for everyday use","category":"men\'s clothing","image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg","rating":{"rate":3.9,"count":120}}',
               200));
 
-      expect(await fetchProducts(client), isA<Products>());
+      expect(await HttpFetch.fetchProducts(), isA<Products>());
     });
     test('throws an exception if the http call completes unsuccessfully', () {
       final client = MockClient();
       when(client.get(Uri.parse('https://fakestoreapi.com/products')))
           .thenAnswer((_) async => http.Response('Not found', 404));
-      expect(fetchProducts(client), throwsException);
+      expect(HttpFetch.fetchProducts(), Exception());
     });
   });
 }
